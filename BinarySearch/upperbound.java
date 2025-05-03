4730ff208c612135f9054e14d7de18d138145524
Athithiyan-arch <athithiyanm87@gmail.com>
2025-05-03 17:53:14 +0530

    lowerbound or upperbound

---
+++ BinarySearch/upperbound.java
@@ -0,0 +1,62 @@
+// import java.util.Scanner;
+// public class upperbound {    
+//     // approach binary search
+//     public static int checkbound (int arr[],int n,int x) {
+//         int low = 0 , high = n - 1;
+//         int ans = n;
+//         while (low <= high) {
+//             int mid = low + high / 2;
+
+//             if (arr[mid] >= x) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             }
+//             else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+//     public static void main(String[] args) {
+//         Scanner obj = new Scanner(System.in);
+//         int x = obj.nextInt();     
+
+//         int arr[] = {3, 5, 8, 15, 19};
+//         int n = 5;
+//         int index = checkbound(arr,n,x);
+//         System.out.println("The lowest index of the upper bound is " + index);
+//     }
+// }
+
+public class upperbound {   
+    // approach linear search
+    public static int lowerBound(int []arr, int n, int x) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= x) {
+                // lower bound found:
+                return i;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = 5, x = 9;
+        int ind = lowerBound(arr, n, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+}
+
+
+/* for binary search
+Time Complexity: O(logN), where N = size of the given array.
+Reason: We are basically using the Binary Search algorithm.
+Space Complexity: O(1) as we are using no extra space.
+*/
+
+/* for linear search the time compicity is 
+Time Complexity: O(N), where N = size of the given array.
+ Reason: In the worst case, we have to travel the whole array. This is basically the time complexity of the linear search algorithm.
+Space Complexity: O(1) as we are using no extra space. */
+
